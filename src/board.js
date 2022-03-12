8e918694de624c0d5f07f5f47a92c3a8c3cd548b
IgorTenoso <lucasmirandaneves@gmail.com>
2022-03-12 20:28:34 -0300

    main card working

--- src/board.js
+++ src/board.js
@@ -1,44 +1,214 @@
-import {idToImage, selectColor} from "./script.js"
 import {pokemonsDb} from "./db.js"
+import {Pokemon} from "./pokemons.js"
 
 
 class Board {
   
   static createPokeLi(pokemon) {
+    const aside     = document.querySelector("aside")
+    const pokeList  = document.getElementById("pokeList")
 
-    /*pokemonsDb.forEach( (pokemon) => {*/
+    let pokemonLi   = document.createElement("li")
+    let pokemonDiv  = document.createElement("div")
+
+    pokemonDiv.innerHTML = `
+
+      <img src="${pokemon.image}" >
       
-      const aside     = document.querySelector("aside")
-      const pokeList  = document.getElementById("pokeList")
-  
-      let pokemonLi = document.createElement("div")
+      <div class="dados" > 
+        <p>${pokemon.name}</p>
+        <p>ID:${pokemon.id}</p>
+      </div>
+    
+    `
 
-      // console.log(pokemon)
-  
-      pokemonLi.innerHTML = `
+    pokemonDiv.classList.add("pokeContainer")
 
-<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" >
-        
-        <div class="dados" > 
-          <p>${pokemon.name}</p>
-          <p>ID:${pokemon.id}</p>
-        </div>
+    pokemonDiv.style.backgroundColor = pokemon.color
+
+    pokemonLi.appendChild(pokemonDiv)
+
+    pokemonLi.id = pokemon.id
+    pokeList.appendChild(pokemonLi)
+    aside.appendChild(pokeList)
+  }
+
+  static getClick(event) {
+
+    let target = event.target
+  
+    for(let i = 0; i < 10; i++){
+
+      if(target.className === "pokeContainer"){
+        target = target.parentNode
+        return target.id
+      }
+
+      else{
+
+        target = target.parentNode
+      }
+
+    }
+
+  }
+
+  static returnRightPokemon(id) {
+
+    for(let i = 0; i < pokemonsDb.length; i++){
+
+      if(pokemonsDb[i].id == id){
+        Board.createPokeCards(pokemonsDb[i - 1], pokemonsDb[i], pokemonsDb[i + 1])
+        console.log(pokemonsDb[i].type)
+      }
+
+    }
+
+  }
+
+  static createPokeCards(pokeLeft, pokeMain, pokeRight) {
+
+    let mainCardContainer       = document.getElementById("mainCardContainer")
+
+    mainCardContainer.innerHTML = ""
+
+    pokeLeft  !== undefined ? Board.createLeftPokeCard(pokeLeft) : Board.createInvisiblePokeCard(pokeMain)
+    Board.createMainPokecard(pokeMain)
+    pokeRight !== undefined ? Board.createLeftPokeCard(pokeRight) : Board.createInvisiblePokeCard(pokeMain)
+
+  }
+
+  static createLeftPokeCard(pokemon){
+    let leftCard            = document.createElement("div")
+    let mainCardContainer   = document.getElementById("mainCardContainer")
+
+    let image   = document.createElement("img")
+      image.src = `${pokemon.image}`
+
+    let div     = document.createElement("div")
+      let h4    = document.createElement("h4")
+      let h2    = document.createElement("h2")
+
+      h4.innerText = `#${pokemon.id}`
+      h2.innerText = `${pokemon.name}`
+  
+      leftCard.appendChild(image)
+    div.appendChild(h4)
+    div.appendChild(h2)
+
+    div.appendChild(Board.createType(pokemon))
+
+    leftCard.appendChild(div)
+
+    div.classList.add("dataContent")
+    leftCard.classList.add("card")
+    mainCardContainer.appendChild(leftCard)
+  }
+
+  static createMainPokecard(pokemon){
+    let mainCard            = document.createElement("div")
+    let mainCardContainer   = document.getElementById("mainCardContainer")
+
+    let image   = document.createElement("img")
+      image.src = `${pokemon.image}`
+
+    let div     = document.createElement("div")
+      let h4    = document.createElement("h4")
+      let h2    = document.createElement("h2")
+
+      h4.innerText = `#${pokemon.id}`
+      h2.innerText = `${pokemon.name}`
+  
+      mainCard.appendChild(image)
+    div.appendChild(h4)
+    div.appendChild(h2)
+
+    div.appendChild(Board.createType(pokemon))
+
+    mainCard.appendChild(div)
+
+    div.classList.add("dataContent")
+    mainCard.classList.add("main")
+    mainCard.classList.add("card")
+    mainCardContainer.appendChild(mainCard)
+  }
+
+  static createRightPokeCard(pokemon){
+    let rightCard            = document.createElement("div")
+    let mainCardContainer   = document.getElementById("mainCardContainer")
+
+    let image   = document.createElement("img")
+      image.src = `${pokemon.image}`
+
+    let div     = document.createElement("div")
+      let h4    = document.createElement("h4")
+      let h2    = document.createElement("h2")
+
+      h4.innerText = `#${pokemon.id}`
+      h2.innerText = `${pokemon.name}`
+  
+      rightCard.appendChild(image)
+    div.appendChild(h4)
+    div.appendChild(h2)
+
+    div.appendChild(Board.createType(pokemon))
+
+    rightCard.appendChild(div)
+
+    div.classList.add("dataContent")
+    rightCard.classList.add("card")
+    mainCardContainer.appendChild(rightCard)
+  }
+
+  static createInvisiblePokeCard(pokemon){
+    let mainCard            = document.createElement("div")
+    let mainCardContainer   = document.getElementById("mainCardContainer")
+
+    mainCard.innerHTML = `
+      <img src="${pokemon.image}" >
+
+      <div class="dataContent" >
+
+        <h4>
+          #${Pokemon.idToImage(pokemon.id)}
+        </h4>
+
+        <h2>
+          ${pokemon.name}
+        </h2>
+
+        <h4 class="electric pokeType" >
+          electric
+        </h4>
+
+      </div>
+    `
+
+    mainCard.classList.add("main")
+    mainCard.classList.add("card")
+    mainCard.classList.add("invisible")
+    mainCardContainer.appendChild(mainCard)
+  }
+
+  static createType(pokemon){
+
+    let ul = document.createElement("ul")
+
+    pokemon.type.forEach( (each) => {
+
+      let li = document.createElement("li")
+      li.innerText = each.type.name
+      li.classList.add(each.type.name)
+      li.classList.add("pokeType")
       
-      `
-      // console.log(pokemon.color)
-  
-      pokemonLi.classList.add("pokeContainer")
-  
-      pokemonLi.style.backgroundColor = pokemon.color
-  
-      pokeList.appendChild(pokemonLi)
-      aside.appendChild(pokeList)
-      // })
+      ul.appendChild(li)
+
+    })
+
+    return ul
 
   }
 
 }
-
-//  <img src="https:/assets.pokemon.com/assets/cms2/img/pokedex/full/${idToImage(pokemon.id)}.png" >
 
 export {Board}
--- src/pokemons.js
+++ src/pokemons.js
@@ -1,12 +1,11 @@
-import {selectColor} from "./script.js"
-
 class Pokemon {
-  constructor(name, id, type, moves){
+  constructor(name, id, type, moves, image){
     this._name  = name
     this._id    = id
     this._type  = type
-    this._color = selectColor(this._type)
+    this._color = Pokemon.selectColor(this._type[0].type.name)
     this._moves = moves 
+    this._image = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${Pokemon.idToImage(this._id)}.png`
   }
 
   get name() {
@@ -29,7 +28,59 @@ class Pokemon {
     return this._moves
   }
 
+  get image() {
+    return this._image
+  }
+
+  static selectColor(type) {
+
+    switch (type) {
+      case "grass":
+        return "#78C850"
+      case "fire":
+        return "#F08030"
+      case "water":
+        return "#6890F0"
+      case "bug":
+        return "#A8B820"
+      case "normal":
+        return "#A8A878"
+      case "flying":
+        return "#3dc7ef"
+      case "poison":
+        return "#A040A0"
+      case "electric":
+        return "#F8D030"
+      case "ground":
+        return "#f7de3f"
+      case "fairy":
+        return "#fdb9e9"
+      case "fighting":
+        return "#d56723"
+      case "psychic":
+        return "#f366b9"
+      case "rock":
+        return "#B8A038"
+      case "ghost":
+        return "#705898"
+      case "ice":
+        return "#98D8D8"
+      case "dragon":
+        return "#53a4cf"
+      case "dark":
+        return "#705B4A"
+    }
+    
+  }
+
+  static idToImage(id) {
+
+    let result = id.toString()
+  
+    result.length === 1 ? result = `00${result}` : result.length === 2 ?  result = `0${result}` : result = result
+  
+    return result.toString()
+  }
 
 }
-
 export {Pokemon}
